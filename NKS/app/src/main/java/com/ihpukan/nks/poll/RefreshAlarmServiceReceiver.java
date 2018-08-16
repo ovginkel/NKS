d7209702cd6fe0f7dfe8dcb8e7eacce33b4dae7c
vginkeo <vginkeo@eskom.co.za>
2018-08-16 16:44:50 +0200

    Use alarm service for notifications

--- NKS/app/src/main/java/com/ihpukan/nks/poll/RefreshAlarmServiceReceiver.java
+++ NKS/app/src/main/java/com/ihpukan/nks/poll/RefreshAlarmServiceReceiver.java
@@ -4,17 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-/**
- * Created by vGinkeO on 2017/08/25.
- */
 
 public class RefreshAlarmServiceReceiver extends BroadcastReceiver {
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        Intent alarmUpdater = new Intent(context, RefreshPollService.class);
+    public void onReceive(Context c, Intent i) {
 
-        context.startService(alarmUpdater);
-        //Log.d("AlarmReceiver", "Called context.startService from AlarmReceiver.onReceive");
+        //onRefreshMessageAlarm();
+
     }
+
 }
