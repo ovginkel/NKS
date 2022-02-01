4ce73a7bb4b949bd70d435adc118d0ee8894eb3c
Oswald van Ginkel <oswald.vanginkel@gmail.com>
2022-02-01 02:21:36 +0200

    AppCompatActivity;

--- NKS/app/src/main/java/com/ihpukan/nks/view/screens/main/navdrawer/NavigationDrawerContract.java
+++ NKS/app/src/main/java/com/ihpukan/nks/view/screens/main/navdrawer/NavigationDrawerContract.java
@@ -1,6 +1,6 @@
 package com.ihpukan.nks.view.screens.main.navdrawer;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.ihpukan.nks.model.Channel;
 import com.ihpukan.nks.model.User;
